52c93ba1d2aee6235e66187bb2a49d76ecd8e42b
ackermannQ <ackermann.quentin@gmail.com>
2021-07-12 17:18:49 +0200

    - Practice class creation and instantiation (no inheritence)

---
+++ Classes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ------------------------------------------------------
+            // You should obviously test any of the objects you write
+            // ------------------------------------------------------
+
+            // To do this exercice, you will need to create three different classes
+            // Person.cs
+            // Cat.cs
+            // Rabbit.cs
+            // These classes will have some common attributes: name, age, sexe, but some attributes and methods will be particular:
+            // A Person has a method Work(int hours) and display in the console "I work ?? hours each day", and has an an attribute string race "human"
+            // A Cat has a method Purr(bool isBeingPet) that display in the console "Rrrrr" if the boolean is set to true, else "Where is my hooman?", and has an an attribute string race "cat"
+            // A Rabbit has a method Binkies(double happiness) that display in the console "I'm so happy I'm doing some binkies" is the happiness is greater that 90%,
+            // or "Give me some food and pet me!" otherwise, and has an an attribute string race "bunbun"
+
+            // When these objects are instantiated, it should be displayed in the console who they are (name, age, sexe and race!)
+            // Then, when you call their specific method, they will perform it
+
+            // Create a person object
+            /* Your code here */
+
+            // Create a cat object
+            /* Your code here */
+
+            // Create two different rabbit objects
+            /* Your code here */
+
+        }
+    }
+}
