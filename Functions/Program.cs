0ad14c3d971282c244cbde671a36b6b51c5927eb
ackermannQ <ackermann.quentin@gmail.com>
2021-07-12 17:08:54 +0200

    - Write functions with different purpose to get a grasp on it

---
+++ Functions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ---------------------------------------------------------
+            // You should obviously test any of the functions you write
+            // ---------------------------------------------------------
+
+            // Write a function with the following prototype: void Presentation(string name, int age)
+            // To present someone
+            /* Your code here */
+
+            // Write a function with the following prototype: int MultiplyByTwo(int number)
+            // To multiply any number input by two
+            /* Your code here */
+
+            // Write a function with the following prototype: int TakeDamage(int damage)
+            // To remove the taken damages from a player
+            // Hint, the player has it life stored in an integer called healthPoint, the initial value is 100
+            /* Your code here */
+
+            // Write a function with the following prototype: int swordAttack()
+            // It should display in the console : Sword Attack!
+            // A sword attack does 20 damages
+            // You defend yourself against a werewolf with 150 points life (int werewolfLife = 150)
+            /* Your code here */
+
+            // How many time do you call the function to defeat the monster?
+            // Could you write a while loop to call the swordAttack() function until the werewolf is vanquished?
+            /* Your code here */
+
+            // Rewrite the swordAttack() function with the following prototype: int swordAttack(int criticAttack)
+            // The purpose is to return an int that is a random number between the least powerfull attack (0) and a critic
+            // Verify that you number is indeed changing each time you call the function
+            /* Your code here */
+
+        }
+    }
+}
